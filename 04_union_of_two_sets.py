6f3adca608e6db06b584b524e4b543d36560915f
sabrin.singh <you@example.com>
2024-07-17 18:24:47 +0545

    added first 4 program

---
+++ 04_union_of_two_sets.py
@@ -0,0 +1,12 @@
+a= {1,2,3}
+b= {1,3,4}
+
+
+
+# Using the union() method
+c = a.union(b)
+print(c)  # Output: {1, 2, 3, 4}
+
+# Using the | operator (which also performs union)
+d = a | b
+print(d)  # Output: {1, 2, 3, 4}
